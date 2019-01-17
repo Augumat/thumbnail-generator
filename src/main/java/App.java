db45a6084df55b78c35ca177cd8bfb23d9e5ff78
Matthew Augustyn <ma62@illinois.edu>
2019-01-17 02:52:10 -0600

    Basics and structure.

---
+++ src/main/java/App.java
@@ -0,0 +1,41 @@
+package main.java;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class App implements Runnable
+{
+    JFrame window;
+    
+    @Override
+    public void run()
+    {
+        //todo implement
+    }
+    
+    private String loadFile(String fileName)
+    {
+        StringBuilder output = new StringBuilder();
+        
+        //Get file from resources folder
+        ClassLoader classLoader = getClass().getClassLoader();
+        File toLoad = new File(classLoader.getResource(fileName).getFile());
+        try
+        {
+            Scanner scanner = new Scanner(toLoad);
+            while (scanner.hasNextLine())
+            {
+                //todo load costume data and character data this way
+//                String line = scanner.nextLine();
+//                output.append(line).append("\n");
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return output.toString();
+    }
+}
