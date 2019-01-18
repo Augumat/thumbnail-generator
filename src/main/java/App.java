89cad1273023e658bb9807d23d752d998c58f3a4
Matthew Augustyn <ma62@illinois.edu>
2019-01-18 02:07:10 -0600

    Made a lot more progress and refined the basic structure of the backend.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,105 +1,252 @@
 package main.java;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
 
 public class App implements Runnable
 {
-    /** */
-    private static final int WIDTH = 500;
-    private static final int HEIGHT = 500;
-    
-    /** The list of all Fighters able to be displayed by the program. */
-    private ArrayList<Fighter> fighters;
+    /** Window size constants. */
+    private static final int WIDTH = 512;
+    private static final int HEIGHT = 512;
     
     /** The main window of the program. */
     private JFrame window;
+    /** The input pane of the application. */
+    private JPanel inputPane;
+    /** The layout of the program. */
+    private LayoutManager layout;
+    
     /** The canvas that previews of the thumbnails will be drawn to. */
     private Canvas canvas;
-    /** The selection box for the first Fighter. */
-    private JComboBox<Fighter> fighterBox1;
-    /** The selection box for the second Fighter. */
-    private JComboBox<Fighter> fighterBox2;
-    /** The selection box for the first Fighter's Variant. */
-    private JComboBox<Variant> variantBox1;
-    /** The selection box for the second Fighter's Variant. */
-    private JComboBox<Variant> variantBox2;
-    /** The input field for inputting a name for the first player. */
-    private JTextField nameBox1;
-    /** The input field for inputting a name for the first player. */
-    private JTextField nameBox2;
-    /** A button to generate and save the thumbnail described by the current state of the input fields. */
-    private JButton generate;
-    /** A button to reset the input fields. */
-    private JButton reset;
-    /** The graphics object used to paint the canvas. */
-    private Graphics2D g;
+    /** A selection box for a Fighter. */
+    private JComboBox<String> fighterBox;
+    /** An input field for inputting a name for a player. */
+    private JTextField nameBox;
+    /** A button. */
+    private JButton button;
     
+    /** The background template. */
+    private BufferedImage bgTemplate;
+    /** The foreground template. */
+    private BufferedImage fgTemplate;
+    
+    /** The left Fighter. */
+    private Fighter fighterLeft;
+    /** The right Fighter. */
+    private Fighter fighterRight;
+    /** The tag of the player on the left. */
+    private String tagLeft;
+    /** The tag of the player on the right. */
+    private String tagRight;
+    /** Which round of the tournament is being played. */
+    private String round;
+    /** The edition of the tournament. */
+    private int edition;
+    /** Whether or not the program is running. */
+    private boolean running;
+    
+    /** The Graphics object used to paint the canvas. */
+    private Graphics2D currentThumbnail;
+    /** The Constraints object for formatting. */
+    private GridBagConstraints c;
+    
+    
+    
+    /** The central method of the application. */
     @Override
     public void run()
     {
+        init();
+        
+        while (running)
+        {
+            if (export())
+            {
+                reset();
+            }
+        }
+        
         //todo implement
-        window = new JFrame();
-        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        window.setSize(WIDTH, HEIGHT);
+
+//        inputPane.setLayout(new GridBagLayout());
+//        c.fill = GridBagConstraints.HORIZONTAL;
+//
+//        //stub test
+//        String[] names = {"Mario", "Fox", "Kirby", "Samus"};
+//        nameBox1 = new JTextField("Player 1", 1);
+//        c.weightx = 0.5;
+//        c.fill = GridBagConstraints.HORIZONTAL;
+//        c.gridx = 0;
+//        c.gridy = 0;
+//        inputPane.add(fighterBox1, c);
+//
+//        button = new JButton("Button 2");
+//        c.fill = GridBagConstraints.HORIZONTAL;
+//        c.weightx = 0.5;
+//        c.gridx = 1;
+//        c.gridy = 0;
+//        inputPane.add(button, c);
+//
+//        button = new JButton("Button 3");
+//        c.fill = GridBagConstraints.HORIZONTAL;
+//        c.weightx = 0.5;
+//        c.gridx = 2;
+//        c.gridy = 0;
+//        inputPane.add(button, c);
+//
+//        button = new JButton("Long-Named Button 4");
+//        c.fill = GridBagConstraints.HORIZONTAL;
+//        c.ipady = 40;      //make this component tall
+//        c.weightx = 0.0;
+//        c.gridwidth = 3;
+//        c.gridx = 0;
+//        c.gridy = 1;
+//        inputPane.add(button, c);
+//
+//        button = new JButton("5");
+//        c.fill = GridBagConstraints.HORIZONTAL;
+//        c.ipady = 0;       //reset to default
+//        c.weighty = 1.0;   //request any extra vertical space
+//        c.anchor = GridBagConstraints.PAGE_END; //bottom of space
+//        c.insets = new Insets(10,0,0,0);  //top padding
+//        c.gridx = 1;       //aligned with button 2
+//        c.gridwidth = 2;   //2 columns wide
+//        c.gridy = 2;       //third row
+//        inputPane.add(button, c);
+        
         
 //        fighterBox1 = new JComboBox<Fighter>(fighters);
 //        fighterBox2 = new JComboBox<Fighter>(fighters);
 //        variantBox1 = new JComboBox<Variant>();
 //        variantBox2 = new JComboBox<Variant>();
-        nameBox1 = new JTextField();
-        nameBox2 = new JTextField();
-        
-        window.setVisible(true);
-        
-        
-//        window.add();
-//        for ()
-//        {
-//
-//        }
+//        nameBox1 = new JTextField("Player 1", 1);
+//        nameBox2 = new JTextField("Player 2", 1);
+//        window.add(nameBox1);
+//        window.add(nameBox2);
+    
+//        window.setLayout(layout);
+//        window.add(inputPane, c);
+//        window.setVisible(true);
     }
     
     /**
-     * Loads a data file from the resources folder.
-     * @param fileName the name of the file to be loaded.
-     * @return the file in raw String form.
+     * The first things called when the app runs.
      */
-    private String loadFile(String fileName)
+    private void init()
     {
-        StringBuilder output = new StringBuilder();
+        //designates the App as running
+        running = true;
         
-        //Get file from resources folder
-        ClassLoader classLoader = getClass().getClassLoader();
-        File toLoad = new File(classLoader.getResource(fileName).getFile());
+        //create window and set basic settings
+        window = new JFrame("Thumbnail Generator v1.0");
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setSize(WIDTH, HEIGHT);
+        
+        //set window icon
+        BufferedImage windowIcon;
         try
         {
-            Scanner scanner = new Scanner(toLoad);
-            while (scanner.hasNextLine())
-            {
-                //todo load costume data and character data this way
-//                String line = scanner.nextLine();
-//                output.append(line).append("\n");
-            }
+            windowIcon = ImageIO.read(new File("src/main/resources/icon.png"));
         }
-        catch (IOException e)
+        catch (java.io.IOException e)
         {
+            System.out.println("[ERROR] Window icon load aborted.");
             e.printStackTrace();
+            windowIcon = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
         }
-        return output.toString();
+        window.setIconImage(windowIcon);
+        
+        //defines layout
+        layout = new GridBagLayout();
+        c = new GridBagConstraints();
+        
+        //initializes and adds components
+        inputPane = new JPanel(layout);
+        c.fill = GridBagConstraints.HORIZONTAL;
+    
+        button = new JButton("Button 2");
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.gridx = 1;
+        c.gridy = 0;
+        inputPane.add(button, c);
+        
+        //loads the basic template and initializes the Graphics object
+        //open
+        try
+        {
+            bgTemplate = ImageIO.read(new File("src/main/resources/bg.png"));
+            fgTemplate = ImageIO.read(new File("src/main/resources/fg.png"));
+        }
+        catch (java.io.IOException e)
+        {
+            System.out.println("[ERROR] Template load aborted.");
+            e.printStackTrace();
+            window.dispose();
+            return;
+        }
+        //close
+        
+        //reveals the window
+        window.setVisible(true);
+        window.requestFocus();
     }
     
-    /**
-     * Exports a thumbnail that was just generated by the app.
-     * @param output The image to be saved.
-     */
-    private static void export(BufferedImage output)
+    private void reset()
     {
         //todo implement
+    }
+    /**
+     * Generates a thumbnail from the accumulated state of the App class if everything is defined and prompts to save
+     * the file to a user-defined location.
+     * @return true if a thumbnail is successfully generated, false otherwise.
+     */
+    private boolean export()
+    {
+        if (isGeneratorReady())
+        {
+            currentThumbnail = bgTemplate.createGraphics();
+            currentThumbnail.drawImage(fighterLeft.getRender(),0,0,639,639,0,0,639,639,null);
+            currentThumbnail.drawImage(fighterLeft.getRender(),0,0,639,639,1279,0,640,639,null);
+            currentThumbnail.drawImage(fgTemplate,0,0,null);
+            try
+            {
+                FileDialog fDialog = new FileDialog(window, "Save", FileDialog.SAVE);
+                fDialog.setVisible(true);
+                String path = fDialog.getDirectory() + fDialog.getFile();
+                ImageIO.write(bgTemplate, "PNG", new File(path));
+            }
+            catch (IOException e)
+            {
+                System.out.println("[ERROR] Failed to write thumbnail to file.");
+                e.printStackTrace();
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+    /**
+     * Checks if all of the information required to generate a thumbnail is
+     * @return true if the generator is ready, false otherwise.
+     */
+    private boolean isGeneratorReady()
+    {
+        if (bgTemplate == null ||
+            fgTemplate == null ||
+            fighterLeft == null ||
+            fighterRight == null ||
+            tagLeft == null ||
+            tagRight == null ||
+            round == null ||
+            edition == 0)
+        {
+            return false;
+        }
+        return true;
     }
 }
--- src/main/java/Fighter.java
+++ src/main/java/Fighter.java
@@ -1,45 +1,275 @@
 package main.java;
 
-import java.util.ArrayList;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 /**
  * Defines a Fighter and stores the images, offsets, and costumes (WIP) associated with them.
  */
 public class Fighter
 {
+    /** The static list of whether each Fighter has different Variant names organized by IDs. */
+    public static boolean[] VARIANT_NAMED =
+            {
+                    false, /* ID:0  Error */
+                    false, /* ID:1  Mario */
+                    false, /* ID:2  Donkey Kong */
+                    false, /* ID:3  Link */
+                    false, /* ID:4  Samus */
+                    false, /* ID:5  Dark Samus */
+                    false, /* ID:6  Yoshi */
+                    false, /* ID:7  Kirby */
+                    false, /* ID:8  Fox */
+                    true , /* ID:9  Pikachu */
+                    false, /* ID:10 Luigi */
+                    false, /* ID:11 Ness */
+                    false, /* ID:12 Captain Falcon */
+                    false, /* ID:13 Jigglypuff */
+                    false, /* ID:14 Peach */
+                    false, /* ID:15 Daisy */
+                    false, /* ID:16 Bowser */
+                    false, /* ID:17 Ice Climbers */
+                    false, /* ID:18 Sheik */
+                    false, /* ID:19 Zelda */
+                    false, /* ID:20 Dr. Mario */
+                    false, /* ID:21 Pichu */
+                    false, /* ID:22 Falco */
+                    false, /* ID:23 Marth */
+                    false, /* ID:24 Lucina */
+                    false, /* ID:25 Young Link */
+                    false, /* ID:26 Ganondorf */
+                    false, /* ID:27 Mewtwo */
+                    false, /* ID:28 Roy */
+                    false, /* ID:29 Chrom */
+                    false, /* ID:30 Mr. Game & Watch */
+                    false, /* ID:31 Meta Knight */
+                    false, /* ID:32 Pit */
+                    false, /* ID:33 Dark Pit */
+                    false, /* ID:34 Zero Suit Samus */
+                    false, /* ID:35 Wario */
+                    false, /* ID:36 Snake */
+                    false, /* ID:37 Ike */
+                    false, /* ID:38 Pokemon Trainer */
+                    false, /* ID:39 Diddy Kong */
+                    false, /* ID:40 Lucas */
+                    false, /* ID:41 Sonic */
+                    false, /* ID:42 King Dedede */
+                    true , /* ID:43 Olimar */
+                    false, /* ID:44 Lucario */
+                    false, /* ID:45 R.O.B. */
+                    false, /* ID:46 Toon Link */
+                    false, /* ID:47 Wolf */
+                    false, /* ID:48 Villager */
+                    false, /* ID:49 Mega Man */
+                    false, /* ID:50 Wii Fit Trainer */
+                    false, /* ID:51 Rosalina & Luma */
+                    false, /* ID:52 Little Mac */
+                    false, /* ID:53 Greninja */
+                    false, /* ID:54 Palutena */
+                    false, /* ID:55 Pac Man */
+                    false, /* ID:56 Robin */
+                    false, /* ID:57 Shulk */
+                    true , /* ID:58 Bowser Jr. */
+                    false, /* ID:59 Duck Hunt */
+                    false, /* ID:60 Ryu */
+                    false, /* ID:61 Ken */
+                    false, /* ID:62 Cloud */
+                    false, /* ID:63 Corrin */
+                    false, /* ID:64 Bayonetta */
+                    false, /* ID:65 Inkling */
+                    true , /* ID:66 Ridley */
+                    false, /* ID:67 Simon */
+                    false, /* ID:68 Richter */
+                    false, /* ID:69 King K. Rool */
+                    false, /* ID:70 Isabelle */
+                    false, /* ID:71 Incineroar */
+                    false, /* ID:72 Mii Brawler */
+                    false, /* ID:73 Mii Swordfighter */
+                    false, /* ID:74 Mii Gunner */
+                    false, /* ID:75 Pirahna Plant */
+                    false  /* ID:76 Joker */
+            };
+    /** The static list of the name of each Fighter organized by IDs. */
+    public static String[][] FIGHTER_NAME =
+            {
+                        {"Null"},             /* ID:0  */
+                        {"Mario"},            /* ID:1  */
+                        {"Donkey Kong"},      /* ID:2  */
+                        {"Link"},             /* ID:3  */
+                        {"Samus"},            /* ID:4  */
+                        {"Dark Samus"},       /* ID:5  */
+                        {"Yoshi"},            /* ID:6  */
+                        {"Kirby"},            /* ID:7  */
+                        {"Fox"},              /* ID:8  */
+                        {"Pikachu", "Pikachu", "Pikachu", "Pikachu", "Pikachu", "Pikachu", "Pikachu", "Pikachu Libre"}, /* ID:9  */
+                        {"Luigi"},            /* ID:10 */
+                        {"Ness"},             /* ID:11 */
+                        {"Captain Falcon"},   /* ID:12 */
+                        {"Jigglypuff"},       /* ID:13 */
+                        {"Peach"},            /* ID:14 */
+                        {"Daisy"},            /* ID:15 */
+                        {"Bowser"},           /* ID:16 */
+                        {"Ice Climbers"},     /* ID:17 */
+                        {"Sheik"},            /* ID:18 */
+                        {"Zelda"},            /* ID:19 */
+                        {"Dr. Mario"},        /* ID:20 */
+                        {"Pichu"},            /* ID:21 */
+                        {"Falco"},            /* ID:22 */
+                        {"Marth"},            /* ID:23 */
+                        {"Lucina"},           /* ID:24 */
+                        {"Young Link"},       /* ID:25 */
+                        {"Ganondorf"},        /* ID:26 */
+                        {"Mewtwo"},           /* ID:27 */
+                        {"Roy"},              /* ID:28 */
+                        {"Chrom"},            /* ID:29 */
+                        {"Mr. Game & Watch"}, /* ID:30 */
+                        {"Meta Knight"},      /* ID:31 */
+                        {"Pit"},              /* ID:32 */
+                        {"Dark Pit"},         /* ID:33 */
+                        {"Zero Suit Samus"},  /* ID:34 */
+                        {"Wario"},            /* ID:35 */
+                        {"Snake"},            /* ID:36 */
+                        {"Ike"},              /* ID:37 */
+                        {"Pokemon Trainer"},  /* ID:38 */
+                        {"Diddy Kong"},       /* ID:39 */
+                        {"Lucas"},            /* ID:40 */
+                        {"Sonic"},            /* ID:41 */
+                        {"King Dedede"},      /* ID:42 */
+                        {"Olimar", "Olimar", "Olimar", "Olimar", "Alph", "Alph", "Alph", "Alph"}, /* ID:43 */
+                        {"Lucario"},          /* ID:44 */
+                        {"R.O.B."},           /* ID:45 */
+                        {"Toon Link"},        /* ID:46 */
+                        {"Wolf"},             /* ID:47 */
+                        {"Villager"},         /* ID:48 */
+                        {"Mega Man"},         /* ID:49 */
+                        {"Wii Fit Trainer"},  /* ID:50 */
+                        {"Rosalina & Luma"},  /* ID:51 */
+                        {"Little Mac"},       /* ID:52 */
+                        {"Greninja"},         /* ID:53 */
+                        {"Palutena"},         /* ID:54 */
+                        {"Pac Man"},          /* ID:55 */
+                        {"Robin"},            /* ID:56 */
+                        {"Shulk"},            /* ID:57 */
+                        {"Boswer Jr.", "Larry", "Roy", "Wendy", "Iggy", "Morton", "Lemmy", "Ludwig"}, /* ID:58 */
+                        {"Duck Hunt"},        /* ID:59 */
+                        {"Ryu"},              /* ID:60 */
+                        {"Ken"},              /* ID:61 */
+                        {"Cloud"},            /* ID:62 */
+                        {"Corrin"},           /* ID:63 */
+                        {"Bayonetta"},        /* ID:64 */
+                        {"Inkling"},          /* ID:65 */
+                        {"Ridley", "Meta Ridley", "Ridley", "Ridley", "Ridley", "Ridley", "Ridley", "Mecha Ridley"}, /* ID:66 */
+                        {"Simon"},            /* ID:67 */
+                        {"Richter"},          /* ID:68 */
+                        {"King K. Rool"},     /* ID:69 */
+                        {"Isabelle"},         /* ID:70 */
+                        {"Incineroar"},       /* ID:71 */
+                        {"Mii Brawler"},      /* ID:72 */
+                        {"Mii Swordfighter"}, /* ID:73 */
+                        {"Mii Gunner"},       /* ID:74 */
+                        {"Pirahna Plant"},    /* ID:75 */
+                        {"Joker"}             /* ID:76 */
+            };
+    /** The total number of Fighters in the game. */
+    public static int FIGHTER_BOUND = 77;
+    /** The total number of Fighters in the game. */
+    public static int VARIANT_BOUND = 8;
+    /** The default variant for every Fighter. */
+    public static int DEFAULT_VARIANT = 0;
+    
     /** The id of this Fighter. */
-    private int id;
-    /** The default name of this Fighter. */
-    private String name;
-    /** All of the Variants that this fighter can wear. */
-    private ArrayList<Variant> variants;
+    private int fighterID;
+    /** The id of the current variant of this Fighter. */
+    private int variantID;
     
     /**
-     * Creates a Fighter object based on an id number referring to a specific folder in resources.
-     * @param initID The id of the Fighter being loaded from file.
+     * Basic constructor.
+     * Creates a Fighter object based on an id number referring to a specific image in resources.
+     * @param initFighter The id of the Fighter being loaded from file.
      */
-    public Fighter(int initID)
+    public Fighter(int initFighter)
     {
-        //todo implement
-        id = initID;
+        if (initFighter > 0 && initFighter < FIGHTER_BOUND)
+        {
+            fighterID = initFighter;
+            variantID = DEFAULT_VARIANT;
+        }
+        else
+        {
+            System.out.println("[ERROR] Invalid fighter id passed.");
+            fighterID = 0;
+            variantID = 0;
+        }
+    }
+    /**
+     * Variant constructor.
+     * Creates a Fighter object based on id numbers referring to a specific image in resources.
+     * @param initFighter The id of the Fighter being created.
+     * @param initVariant The id of the Variant of the Fighter being created.
+     */
+    public Fighter(int initFighter, int initVariant)
+    {
+        if (initFighter > 0 && initFighter < FIGHTER_BOUND)
+        {
+            fighterID = initFighter;
+            if (initVariant >= 0 && initVariant < VARIANT_BOUND)
+            {
+                variantID = initVariant;
+            }
+            else
+            {
+                System.out.println("[ERROR] Invalid variant id passed.");
+                variantID = DEFAULT_VARIANT;
+            }
+        }
+        else
+        {
+            System.out.println("[ERROR] Invalid fighter id passed.");
+            fighterID = 0;
+            variantID = 0;
+        }
+    }
     
-        
+    /** Getter for fighter id. */
+    public int getFighterID()
+    {
+        return fighterID;
+    }
+    /** Getter for variant id. */
+    public int getVariantID()
+    {
+        return variantID;
     }
     
     /**
-     * Getter for name.
-     * @return The default name of this Fighter.
+     * Returns a render of this Fighter in the form of a BufferedImage.
+     * @return the render of the fighter described by this instance of Fighter.
      */
-    public String getName()
+    public BufferedImage getRender()
     {
-        return name;
+        BufferedImage output;
+        try
+        {
+            output = ImageIO.read(new File("src/main/resources/" + fighterID + "/" + variantID + "/render.png"));
+        }
+        catch (java.io.IOException e)
+        {
+            System.out.println("[ERROR] Render load aborted.");
+            e.printStackTrace();
+            output = new BufferedImage(1,1, BufferedImage.TYPE_INT_ARGB);
+        }
+        return output;
     }
-    /**
-     * Getter for id.
-     * @return The id of this Fighter.
-     */
-    public int getID()
+    
+    /** Evaluates to the name of the Fighter's default Variant. */
+    @Override
+    public String toString()
     {
-        return id;
+        if (VARIANT_NAMED[fighterID])
+        {
+            return FIGHTER_NAME[fighterID][variantID];
+        }
+        return FIGHTER_NAME[fighterID][0];
     }
 }
